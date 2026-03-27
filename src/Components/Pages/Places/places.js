44a93b179fdd1de9a0bfe674ab45f650515d98af
code <code@Adantas-MacBook-Air.local>
2026-03-27 18:03:41 -0400

    deploy edits

--- src/Components/Pages/Places/places.js
+++ src/Components/Pages/Places/places.js
@@ -7,26 +7,26 @@ function Places(){
         <section className={styles.places}>
             <div className={styles.placesContainer}>
                 <div className={styles.visited}>
-                    <div id={styles.big} className={styles.spot}><img src="fpark.jpeg"/></div>
+                    <div id={styles.big} className={styles.spot}><img alt="" src="fpark.jpeg"/></div>
                     <div id={styles} className={styles.spot}>b</div>
-                    <div id={styles} className={styles.spot}><img src="qfc.png"/></div>
-                    <div id={styles.big} className={styles.spot}><img src="skydive.jpg"/></div>
-                    <div id={styles} className={styles.spot}><img src="kyoto.jpg"/></div>
+                    <div id={styles} className={styles.spot}><img alt="" src="qfc.png"/></div>
+                    <div id={styles.big} className={styles.spot}><img alt="" src="skydive.jpg"/></div>
+                    <div id={styles} className={styles.spot}><img alt="" src="kyoto.jpg"/></div>
                     <div id={styles.small} className={styles.spot}>f</div>
                     <div id={styles} className={styles.spot}>g</div>
                     <div id={styles} className={styles.spot}>h</div>
-                    <div id={styles.small} className={styles.spot}><img src="paris.avif"/></div>
+                    <div id={styles.small} className={styles.spot}><img alt="" src="paris.avif"/></div>
                     <div id={styles} className={styles.spot}>j</div>
-                    <div id={styles.big} className={styles.spot}><img src="cirque.jpg"/></div>
+                    <div id={styles.big} className={styles.spot}><img alt="" src="cirque.jpg"/></div>
                     <div id={styles} className={styles.spot}>l</div>
-                    <div id={styles} className={styles.spot}><img src="fogo.jpg"/></div>
+                    <div id={styles} className={styles.spot}><img alt="" src="fogo.jpg"/></div>
                     <div id={styles.small} className={styles.spot}>n</div>
                     <div id={styles} className={styles.spot}>o</div>
                     <div id={styles} className={styles.spot}>p</div>
-                    <div id={styles.big} className={styles.spot}><img src="sparta.jpeg"/></div>
+                    <div id={styles.big} className={styles.spot}><img alt="" src="sparta.jpeg"/></div>
                     <div id={styles} className={styles.spot}>r</div>
                     <div id={styles} className={styles.spot}>s</div>
-                    <div id={styles.big} className={styles.spot}><img src="kaku.jpg"/></div>
+                    <div id={styles.big} className={styles.spot}><img alt="" src="kaku.jpg"/></div>
                     <div id={styles} className={styles.spot}>u</div>
                     <div id={styles} className={styles.spot}>v</div>
                     <div id={styles.small} className={styles.spot}>w</div>
